582b7293f80d3f7f0ccf1fc0a2ef088c67c2351c
dola-code118 <dola37608>
2026-03-09 09:20:52 +0600

    added for the first time JavaScript DOM to github

---
+++ main.js
@@ -0,0 +1,6 @@
+const title = document.getElementById("title");
+const description= document.getElementById("desc");
+const changeBtn= document.getElementById("change-button");
+changeBtn.addEventListener("click,function(){
+		title.textContent="Amra prothombarer moto Event Listener kora shikhlam"
+})
